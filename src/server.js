17ff1c92fd1a5d32cb7029da30786a94d484f9bf
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-04-15 17:17:36 -0300

    Adicionando socket

--- src/server.js
+++ src/server.js
@@ -2,12 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const urlConnectMongo = require("./config/urlConnectMongo");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connectionRoom', box => {
+    socket.join(box);
+  })
+})
 
 mongoose.connect(urlConnectMongo, {
   useNewUrlParser: true
 });
+
+app.use((req, res, next) => {
+  req.io = io;
+  
+  return next();
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +32,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
